ccf65658e8af80221a2169ecf919ff4b640dbd02
ZAKI <zakiyoszack@gmail.com>
2024-10-06 19:11:47 +0300

    axios and requiest

---
+++ netflix-clone/src/utils/requests.jsx
@@ -0,0 +1,10 @@
+const API_KEY= process.env.REACT_APP_API_KEY
+const requests ={
+    actionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    comedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    tvShows: `/discover/tv?api_key=${API_KEY}`,
+    actionTvShows: `/discover/tv?api_key=${API_KEY}&with_genres=10759`
+
+
+
+}
